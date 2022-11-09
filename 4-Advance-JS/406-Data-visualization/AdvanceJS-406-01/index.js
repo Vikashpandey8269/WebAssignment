69b8be73f2220e31f105afc087390cf573550c11
Vikash Pandey <pandeyvikash6991@gmail.com>
2022-11-09 22:12:03 +0530

    done

--- 4-Advance-JS/406-Data-visualization/AdvanceJS-406-01/index.js
+++ 4-Advance-JS/406-Data-visualization/AdvanceJS-406-01/index.js
@@ -4,25 +4,36 @@ const showres=(e)=>{
     display(e.target.value);
  }
  function display(element){
-     const filter=countries.filter((value)=>{
-         if(value.(element)){
+     const filter=countries_data.filter((value)=>{
+         if(value.name.startsWith(element)){
              return true;
          }
          return false;
      });
  
-     const parent = document.getElementById("table");
+   
+     const parent = document.getElementById('grid');
+
      parent.innerHTML="";
+
      filter.forEach((e)=>{
-         const new_row=document.createElement("tr");
-         const new_countries=document.createElement("td");
+
+
+        const new_row=document.createElement("div");
+         const new_countries=document.createElement("p");
+
+       
+
          new_countries.innerHTML=e.name;
          new_row.appendChild(new_countries);
          parent.appendChild(new_row);
+         const country=document.getElementById("no-of-country");
+         country.innerHTML=`countries start with ${element} are ${filter.length}`;
  
      });
  }
- 
- document.getElementById("search-form").addEventListener("keyup",showres);
-    
 
+
+ document.getElementById("search-form").addEventListener("keyup",showres);
+ 
+ 
